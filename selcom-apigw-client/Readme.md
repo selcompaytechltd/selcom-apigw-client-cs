86350eb901fb5acf59d6e5419f1526de4d674131
gradoMabachi <grado@selcom.net>
2023-02-28 09:23:21 +0300

    initial upload

---
+++ selcom-apigw-client/Readme.md
@@ -0,0 +1,85 @@
+ 
+<h1 align="center">SELCOM API CLIENT</h1>
+
+<p align='center'>
+
+<img src="https://img.shields.io/badge/net%20-v6.0-blue">
+
+<img src="https://img.shields.io/badge/C%23-asp.net-blue">
+</p >
+## Homepage
+https://developers.selcommobile.com/
+
+## Description
+This is a library containing functions that aid in the accessing of selcom api. IT is made up pf 4 functions.
+computeHeader 
+
+## Installation 
+dotnet add package  selcom-apigw-client
+
+### Use
+
+```cs
+//use package
+using selcom_apgw_client;
+
+//// initalize a new apiAccess instace with values of the base url, api key and api secret
+
+apigwClient client = new apigwClient(baseUrl, apiKey, apiSecret);
+
+// computeHeader a dictionary containing data to bes submitted
+// computeHeader returns an array with values for the following header fields: 
+// Authorization, Timestamp, Digest, Signed-Fields
+client.computeHeader( jsonData):
+
+// postFuct takes relative path to base url. dictionary containing data to be submitted 
+// It performs a POST request of the submitted data to the destniation url generatingg the header internally
+// IT returns a String containing the response data to the request
+client.postFunc(path, jsonData)
+
+// getFuct takestakes relative path to base url. dictionary containing data to be submitted  
+// It performs a GET request adding the query to the  url and generatingg the header internally
+// IT returns a Stringcontaining the response data to the request
+client.getFunc(path, jsonData)
+
+// deletetFuct takes relative path to base url.dictionary containing data to be submitted 
+// It performs a DELETE request adding the query to the  url and generatingg the header internally
+// IT returns a String containing the response data to the request
+client.deleteFunc(path, jsonData)
+```
+### Examples
+```js
+//import package
+using selcom_apgw_client;
+// initalize a new apiAccess instace with values of the base url, api key and api secret
+
+String apiKey = '202cb962ac59075b964b07152d234b70';
+String apiSecret = '81dc9bdb52d04dc20036dbd8313ed055';
+String baseUrl = "http://example.com";
+
+
+
+// initalize a new apiAccess instace with values of the base url, api key and api secret
+var client = new apigwClient(baseUrl, apiKey, apiSecret);
+
+//order data
+Dictionary<String, Object> orderDict = new Dictionary<String,Object>() ;
+orderDict.Add("vendor","VENDORTILL");
+orderDict.Add("order_id","1218d5Qb");
+orderDict.Add("buyer_email", "john@example.com");
+orderDict.Add("buyer_name", "John Joh");
+orderDict.Add("buyer_phone","255682555555");
+orderDict.Add("amount",  8000,);
+orderDict.Add("currency","TZS");
+orderDict.Add("buyer_remarks","None");
+orderDict.Add("merchant_remarks","None");
+orderDict.Add("no_of_items",  1);
+
+
+// path relatiive to base url
+String orderPath = "/v1/checkout/create-order-minimal";
+//crate new order
+
+var = orderRespose = client.postFunc(orderPath, orderArray);
+Console.Write(orderResponse);
+```
---
+++ selcom-apigw-client/apigwClient.cs
@@ -0,0 +1,163 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace selcom_apigw_client
+{
+    public class apigwClient
+    {
+        private String baseUrl;
+        private String apiKey;
+        private String apiSecret;
+
+        public apigwClient(String baseUrl, String apiKey, String apiSecret)
+        {
+            this.baseUrl = baseUrl;
+            this.apiKey = apiKey;
+            this.apiSecret = apiSecret;
+        }
+
+        public Dictionary<String, String> computeHeader(Dictionary<String, Object> dictData)
+        {
+            byte[] bapiKey = Encoding.Default.GetBytes(apiKey);
+            String authToken = "SELCOM " + Convert.ToBase64String(bapiKey);
+
+            String timestamp = DateTime.Now.ToString("yyyy-MM-dd'T'HH:mm:sszzzz");
+
+            String signedFields = "";
+            String digestData = "timestamp=" + timestamp;
+
+
+            foreach (KeyValuePair<string, Object> entry in dictData)
+            {
+                digestData = digestData + "&" + entry.Key + "=" + entry.Value.ToString();
+                if (string.IsNullOrEmpty(signedFields))
+                { signedFields = signedFields + entry.Key; }
+                else
+                { signedFields = signedFields + "," + entry.Key; }
+            }
+            
+            byte[] bapiSecret = Encoding.Default.GetBytes(apiSecret);
+            byte[] bdigestData = Encoding.Default.GetBytes(digestData);
+
+            HMACSHA256 hmac = new HMACSHA256(bapiSecret);
+            byte[] bhasdheData = hmac.ComputeHash(bdigestData);
+            String digest = Convert.ToBase64String(bhasdheData);
+            Dictionary<String, String> headers = new Dictionary<String, String>();
+            headers.Add("authToken", authToken);
+            headers.Add("timestamp", timestamp);
+            headers.Add("digest", digest);
+            headers.Add("signedFields", signedFields);
+            return headers;
+
+        }
+
+        public String postFunc(String path, Dictionary<String, Object> dictData)
+        {
+            Dictionary<String, String> headers = computeHeader(dictData);
+            var client = new HttpClient();
+            String  url = baseUrl + path;
+
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri(url),
+                Method = HttpMethod.Post,
+
+            };
+
+
+            request.Headers.Add("Authorization", headers["authToken"]);
+            request.Headers.Add("Digest-Method", "HS256");
+            request.Headers.Add("Digest", headers["digest"]);
+            request.Headers.Add("Timestamp", headers["timestamp"]);
+            request.Headers.Add("Signed-Fields", headers["signedFields"]);
+
+            var jsonData = JsonConvert.SerializeObject(dictData);
+            var content = new StringContent(jsonData.ToString(), Encoding.UTF8, "application/json");
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            request.Content = content;
+            var respnse = client.SendAsync(request).GetAwaiter().GetResult();
+            var respnseData = respnse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            return respnseData;
+
+
+
+        }
+
+        public String getFunc(String path, Dictionary<String, Object> dictData)
+        {
+            Dictionary<String, String> headers = computeHeader(dictData);
+            var client = new HttpClient();
+            String url = baseUrl + path;
+
+
+            var parameters = "";
+            foreach (KeyValuePair<string, Object> entry in dictData)
+            {
+                if (string.IsNullOrEmpty(parameters))
+                { parameters = "?" + entry.Key + "=" + entry.Value.ToString(); }
+                else
+                { parameters = parameters + "&" + entry.Key + "=" + entry.Value.ToString(); }
+            }
+            url = url + parameters;
+
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri(url),
+                Method = HttpMethod.Get,
+
+            };
+            request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.Add("Authorization", headers["authToken"]);
+            request.Headers.Add("Digest-Method", "HS256");
+            request.Headers.Add("Digest", headers["digest"]);
+            request.Headers.Add("Timestamp", headers["timestamp"]);
+            request.Headers.Add("Signed-Fields", headers["signedFields"]);
+            var respnse = client.SendAsync(request).GetAwaiter().GetResult();
+            var respnseData = respnse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            return respnseData;
+
+
+        }
+
+        public String deleteFunc(String path, Dictionary<String, Object> dictData)
+        {
+            Dictionary<String, String> headers = computeHeader(dictData);
+            var client = new HttpClient();
+            String url = baseUrl + path;
+
+
+            var parameters = "";
+            foreach (KeyValuePair<string, Object> entry in dictData)
+            {
+                if (string.IsNullOrEmpty(parameters))
+                { parameters = "?" + entry.Key + "=" + entry.Value.ToString(); }
+                else
+                { parameters = parameters + "&" + entry.Key + "=" + entry.Value.ToString(); }
+            }
+            url = url + parameters;
+
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri(url),
+                Method = HttpMethod.Delete,
+
+            };
+            request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.Add("Authorization", headers["authToken"]);
+            request.Headers.Add("Digest-Method", "HS256");
+            request.Headers.Add("Digest", headers["digest"]);
+            request.Headers.Add("Timestamp", headers["timestamp"]);
+            request.Headers.Add("Signed-Fields", headers["signedFields"]);
+
+            var respnse = client.SendAsync(request).GetAwaiter().GetResult();
+            var respnseData = respnse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            return respnseData;
+
+
+
+        }
+    }
+
+}
